7da1b1f77be66d06501eb1212c901af7b6b562a5
unknown <juan.cosen2004@gmail.com>
2023-11-01 14:08:33 -0300

    agrego login y register

---
+++ js/inicio-sesion.js
@@ -0,0 +1,46 @@
+const inputMail = document.getElementById('idUser')
+const inputPass = document.getElementById('idPass')
+const buttonLog = document.getElementById('buttonLogin') 
+
+const objForm = {
+    user: '',
+    pass: ''
+} 
+
+const formValues = (ev) => {
+    const { name, value } = ev.target   
+    objForm[name] = value 
+}
+
+const sendForm = (ev) => {
+    ev.preventDefault()
+    const { user , pass } = objForm
+
+    if(user && pass){
+        console.log('click')
+        const users =  JSON.parse(localStorage.getItem('users')) || []
+        const userExist = users.filter((userLS) => userLS.user === user)
+        console.log(userExist)
+
+        if(userExist.length > 0){
+            return alert('usuario y/o contraseña incorrecta')
+        }
+        
+        if(userExist[0].pass === pass){
+            userExist[0].login = true
+            localStorage.setItem('users', JSON.stringify(users))
+
+            if(userExist[0].rol === 'admin'){
+                location.href = '#'
+            }else{
+                location.href = '#'
+            }
+        }else{
+            alert('usuario y/o contraseña incorrecta')
+        }
+    }
+}
+
+inputMail.addEventListener('input', formValues)
+inputPass.addEventListener('input', formValues)
+buttonLog.addEventListener('click', sendForm)
